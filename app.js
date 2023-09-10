ba26f554d51a7e6aeb26d19aa3dbc1942d8f422e
Aamin887 <forkhamin@yahoo.co.uk>
2023-09-10 15:09:50 +0100

    add play round

--- app.js
+++ app.js
@@ -4,3 +4,35 @@ function getComputerChoice(){
     const choice = Math.floor(Math.random() * playOptions.length);
     return playOptions[choice];
 }                             
+
+// play round
+function playRound(playerSelection, computerSelection){
+    let formattedPlayerSelection = playerSelection.toLowerCase();
+    if(computerSelection === 'rock'){
+        if(formattedPlayerSelection === 'scissor'){
+            return `You lose! Rock beats scissor`;
+        }else if(formattedPlayerSelection === 'paper'){
+            return `You win! Paper beats rock`;
+        }else{
+            return `It's tie.`
+        }
+    }else if(computerSelection === 'paper'){
+        if(formattedPlayerSelection === 'scissor'){
+            return `You win! Scissor beats paper`;
+        }else if(formattedPlayerSelection === 'rock'){
+            return `You lose! Paper beats rock`;
+        }else{
+            return `It's tie.`
+        }
+    }else if(computerSelection === 'scissor'){
+        if(formattedPlayerSelection === 'rock'){
+            return `You win! Rock beats scissor`;
+        }else if(formattedPlayerSelection === 'paper'){
+            return `You lose! scissor beats paper`;
+        }else{
+            return `It's tie.`
+        }
+    }
+}
+
+console.log(playRound('rock', getComputerChoice()))
